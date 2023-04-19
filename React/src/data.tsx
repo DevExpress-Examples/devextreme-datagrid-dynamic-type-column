2610adf17157f0447f562e464358e3aa28974b3d
AssylbekDanyshbek <danyshbek1996@gmail.com>
2023-04-19 17:49:13 +0600

    React sample

---
+++ React/src/data.tsx
@@ -0,0 +1,63 @@
+
+interface MyCustomType {
+  ID: number;
+  Name: String; 
+}
+export interface SampleItem {
+  ID: number;
+  DynamicValue: string | number | MyCustomType | Date | boolean | null;
+  Type: String;
+}
+
+const sampleData = [
+    {
+      ID: 1,
+      DynamicValue: "Sample String",
+      Type: "String"
+    },
+    {
+      ID: 2,
+      DynamicValue: 42,
+      Type: "Number"
+    },
+    {
+      ID: 3,
+      DynamicValue: new Date(),
+      Type: "Date"
+    },
+    {
+      ID: 4,
+      DynamicValue: true,
+      Type: "Boolean"
+    },
+    {
+      ID: 5,
+      DynamicValue: {
+        ID: 0,
+        Name: "John Doe"
+      },
+      Type: "MyCustomType"
+    }
+];
+
+const myDropdownData: MyCustomType[] = [
+    {
+      ID: 0,
+      Name: 'John Doe'
+    },
+    {
+      ID: 1,
+      Name: 'Jane Smith'
+    }
+];
+
+const service = {
+    getData(){
+        return sampleData;
+    },
+    getDropDownData(){
+        return myDropdownData;
+    }
+}
+
+export default service;
