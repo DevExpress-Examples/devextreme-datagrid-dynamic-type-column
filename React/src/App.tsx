a7f3dcb092adb9078bdc095bdd7408966abb050a
Mark Allen Ramirez <markallenramirez@gmail.com>
2023-06-13 13:01:30 +0800

    fix lint react

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,58 +1,57 @@
 import './App.css';
 import 'devextreme/dist/css/dx.material.blue.light.compact.css';
-import { useCallback } from 'react';
-import service, { SampleItem } from "./data";
-
-import DataGrid, {Editing, Column, Lookup} from 'devextreme-react/data-grid';
-
+import React, { useCallback } from 'react';
+import DataGrid, { Editing, Column, Lookup } from 'devextreme-react/data-grid';
+import { InitNewRowEvent } from 'devextreme/ui/data_grid';
+import service, { SampleItem } from './data';
 import DynamicEditCellComponent from './components/DynamicEditCellComponent';
 import CellComponent from './components/CellComponent';
 
-import {   InitNewRowEvent } from 'devextreme/ui/data_grid';
-
-const typeList = ["String","Number","Date","Boolean", "MyCustomType"];
+const typeList = ['String', 'Number', 'Date', 'Boolean', 'MyCustomType'];
 const ds = service.getData();
-function App() {
-  const typeCellValue = useCallback((newData: SampleItem, type:String) => {
+
+function App(): React.ReactElement {
+  const typeCellValue = useCallback((newData: SampleItem, type: String): void => {
     newData.Type = type;
     newData.DynamicValue = null;
   }, []);
 
-  const onInitNewRow = useCallback((e:InitNewRowEvent<SampleItem, number>) =>{
-    const newKey = Math.max(...service.getData().map(item=>item.ID)) + 1;
+  const onInitNewRow = useCallback((e: InitNewRowEvent<SampleItem, number>): void => {
+    const newKey = Math.max(...service.getData().map((item) => item.ID)) + 1;
     e.data.ID = newKey;
-    e.data.Type = "defaultType";
-    e.data.DynamicValue = "defaultValue";
+    e.data.Type = 'defaultType';
+    e.data.DynamicValue = 'defaultValue';
   }, []);
 
   return (
-    <div className="main">
+    <div className='main'>
       <DataGrid<SampleItem, number>
         dataSource={ds}
         keyExpr='ID'
         showBorders={true}
         onInitNewRow={onInitNewRow}
-        repaintChangesOnly={true}>
-          <Editing
-            allowUpdating={true}
-            allowAdding={true}
-            mode="form"></Editing>
-          <Column
-            dataField="ID"
-            allowEditing={false}
-          ></Column>
-          <Column
-            dataField="DynamicValue"
-            dataType="object"
-            editCellComponent={DynamicEditCellComponent}
-            cellComponent={CellComponent}
-          ></Column>
-          <Column
-            dataField="Type"
-            setCellValue={typeCellValue}
-          >
-            <Lookup dataSource={typeList}></Lookup>
-          </Column>
+        repaintChangesOnly={true}
+      >
+        <Editing
+          allowUpdating={true}
+          allowAdding={true}
+          mode='form'></Editing>
+        <Column
+          dataField='ID'
+          allowEditing={false}
+        ></Column>
+        <Column
+          dataField='DynamicValue'
+          dataType='object'
+          editCellComponent={DynamicEditCellComponent}
+          cellComponent={CellComponent}
+        ></Column>
+        <Column
+          dataField='Type'
+          setCellValue={typeCellValue}
+        >
+          <Lookup dataSource={typeList}></Lookup>
+        </Column>
       </DataGrid>
     </div>
   );
--- React/src/components/CellComponent.tsx
+++ React/src/components/CellComponent.tsx
@@ -1,23 +1,23 @@
-import { ColumnCellTemplateData } from "devextreme/ui/data_grid"
-import { SampleItem } from "../data"
+import React from 'react';
+import { ColumnCellTemplateData } from 'devextreme/ui/data_grid';
+import { SampleItem } from '../data';
 
-
-function CellComponent({ data } : { data: ColumnCellTemplateData<SampleItem, number> }){
-    const {data: rowData, value} = data;
-    switch (rowData?.Type) {
-        case "Number":
-            return (<div>{value.toString()}</div>)
-        case "String":
-            return (<div>{value}</div>)
-        case "Date":
-            return (<div>{new Date(value).toLocaleDateString()}</div>)
-        case "Boolean":
-            return (<div>{value.toString()}</div>)
-        case "MyCustomType":
-            return (<div>{value ? value.Name : ""}</div>)
-        default:
-            return (<div>unknown data type</div>)
-    }
+function CellComponent({ data }: { data: ColumnCellTemplateData<SampleItem, number> }): React.ReactElement {
+  const { data: rowData, value } = data;
+  switch (rowData?.Type) {
+    case 'Number':
+      return (<div>{value.toString()}</div>);
+    case 'String':
+      return (<div>{value}</div>);
+    case 'Date':
+      return (<div>{new Date(value).toLocaleDateString()}</div>);
+    case 'Boolean':
+      return (<div>{value.toString()}</div>);
+    case 'MyCustomType':
+      return (<div>{value ? value.Name : ''}</div>);
+    default:
+      return (<div>unknown data type</div>);
+  }
 }
 
 export default CellComponent;
